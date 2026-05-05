b4129040c9505922fc53f42e738cc9dcc8df04d0
jjeuwi <marcialgarljoshua@gmail.com>
2026-05-05 18:57:05 +0800

    fixing deployment bugs

--- server/src/index.js
+++ server/src/index.js
@@ -30,8 +30,8 @@ if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile()
-    })
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 
